dad4fa18c808abf039052a09f6bf5343501d8dae
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2021-04-12 17:45:52 -0500

    Parameterize the api key in the publication script

--- scripts/Publish-PowerSugar.ps1
+++ scripts/Publish-PowerSugar.ps1
@@ -1,11 +1,17 @@
+#Requires -Version 7.1
+[CmdletBinding(PositionalBinding = $false, SupportsShouldProcess)]
 param(
-    [string]
+    [Parameter(Mandatory)]
+    [Alias('key')]
+    [SecureString]
+    $NuGetApiKey,
+
+    [Alias('root')]
     [ValidateScript({ Test-Path -LiteralPath $_ -PathType Container })]
+    [string]
     $RepoRoot = $(Split-Path -LiteralPath $PSScriptRoot)
 )
 
 Publish-Module `
     -Path $(Join-Path $RepoRoot 'src' 'PowerSugar') `
-    -NuGetApiKey $(ConvertFrom-SecureString -AsPlainText $(
-        Import-CliXml -LiteralPath $(Join-Path $HOME '.keys' 'powershellgallery' "$((Get-ComputerInfo).CsName).clixml")
-    ))
+    -NuGetApiKey $(ConvertFrom-SecureString -AsPlainText $NuGetApiKey)

121c2c738a80694d0733009a67b06811294a4ed3
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2021-05-17 19:35:10 -0500

    Test Get-FileDate (alias: ftoday)

---
+++ tests/Date/Get-FileDate.tests.ps1
@@ -0,0 +1,33 @@
+#Requires -Module Pester
+
+BeforeAll {
+    Import-Module 'PowerSugar'
+}
+
+Describe 'Get-FileDate' {
+    It 'is exported from PowerSugar' {
+        (Get-Module 'PowerSugar').ExportedCommands.Keys | Should -Contain 'Get-FileDate'
+    }
+    It 'is aliased as "ftoday"' {
+        Get-Alias -Definition 'Get-FileDate' -ErrorAction Ignore | Should -Be 'ftoday'
+    }
+    Context 'when invoking it via its alias' {
+        It 'returns a string of the FileDate format' {
+            $fileDate = ftoday
+            $fileDate | Should -BeOfType ([string])
+            $fileDate | Should -Match '^\d{4}\d{2}\d{2}$' -Because 'the FileDate format is "yyyymmdd"'
+        }
+        It 'returns the current FileDate' {
+            $olderFileDate = Get-Date -Format FileDate
+            $currentFileDate = ftoday
+            $newerFileDate = Get-Date -Format FileDate
+
+            $currentFileDate | Should -BeGreaterOrEqual $olderFileDate
+            $currentFileDate | Should -BeLessOrEqual $newerFileDate
+        }
+    }
+}
+
+AfterAll {
+    Remove-Module 'PowerSugar'
+}
